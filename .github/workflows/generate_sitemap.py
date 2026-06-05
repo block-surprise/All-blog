46ac75fce43ea13788258fa8ef9d5233a75d9e47
block-surprise <miyazaki902@yahoo.co.jp>
2026-06-05 19:39:24 +0900

    Update generate_sitemap.py

--- .github/workflows/generate_sitemap.py
+++ .github/workflows/generate_sitemap.py
@@ -3,31 +3,44 @@ import os
 BASE_URL = "https://htn-news.f5.si"
 POST_DIR = "posts"
 
-files = sorted(os.listdir(POST_DIR), reverse=True)
-
 urls = ""
 
-for f in files:
-    if f.endswith(".md"):
-        urls += f"""
-  <url>
-    <loc>{BASE_URL}/posts/{f}</loc>
-  </url>
+for root, dirs, files in os.walk(POST_DIR):
+
+    for f in files:
+
+        if f.endswith(".md"):
+
+            path = os.path.join(root, f).replace("\\", "/")
+
+            urls += f"""
+
+<url>
+
+  <loc>{BASE_URL}/{path}</loc>
+
+</url>
+
 """
 
 xml = f"""<?xml version="1.0" encoding="UTF-8"?>
+
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-  <url>
-    <loc>{BASE_URL}</loc>
-  </url>
+<url>
+
+  <loc>{BASE_URL}</loc>
+
+</url>
 
 {urls}
 
 </urlset>
+
 """
 
 with open("sitemap.xml", "w", encoding="utf-8") as f:
+
     f.write(xml)
 
-print("sitemap generated")
+print("sitemap updated")
