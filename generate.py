21738c47a3adb6415940548e36ee4196a9e14db2
block-surprise <miyazaki902@yahoo.co.jp>
2026-06-07 13:34:46 +0900

    Update generate.py

--- generate.py
+++ generate.py
@@ -256,6 +256,7 @@ def build_html(title, body, category, image_url):
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>{title}</title>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3571574988222927"crossorigin="anonymous"></script>
 <link rel="icon" type="image/png" href="/favicon.png">
 <link rel="apple-touch-icon" href="/favicon.png">
 <style>
