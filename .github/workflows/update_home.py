e5273f0eacda3250f0434901a85a24f41b129634
block-surprise <miyazaki902@yahoo.co.jp>
2026-06-07 10:01:00 +0900

    Update update_home.py

--- .github/workflows/update_home.py
+++ .github/workflows/update_home.py
@@ -121,6 +121,8 @@ html = f"""
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>ひとりテックニュース</title>
 
+<link rel="icon" type="image/png" href="/favicon.png">
+<link rel="apple-touch-icon" href="/favicon.png">
 <style>
 body {{
     margin: 0;
