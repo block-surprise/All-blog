55dc80216a8cb7712f71bdda4001f2b71a740203
block-surprise <miyazaki902@yahoo.co.jp>
2026-06-09 07:29:27 +0900

    Update search_builder.py

--- search_builder.py
+++ search_builder.py
@@ -154,27 +154,17 @@ header {{
 
     print("search page created:", filename)
     
-def build_all_search_pages(articles):
+def build_all_search_pages(articles, keywords):
 
     os.makedirs("game/search", exist_ok=True)
-
-    keywords = set()
-
-    for a in articles:
-        for word in a["title"].split():
-            if len(word) >= 2:
-                keywords.add(word)
 
     # 古い検索ページ削除
     for f in os.listdir("game/search"):
         if f.endswith(".html"):
             os.remove(os.path.join("game/search", f))
 
-    # 新規生成
+    # 手動キーワードで生成
     for kw in keywords:
-
-        filename = safe_filename(kw)
-
         build_search_page(kw, articles)
 
-    print("all search pages rebuilt:", len(keywords))
+    print("search pages created:", len(keywords))
