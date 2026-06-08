eae61161012a5008154858f9e20410db17b4a1aa
block-surprise <miyazaki902@yahoo.co.jp>
2026-06-08 22:27:09 +0900

    Update add_latest.py

--- add_latest.py
+++ add_latest.py
@@ -1,13 +1,12 @@
 import os
 
 INSERT_HTML = """
-
 <h2>最新記事</h2>
 <div id="latest-posts">
 読み込み中...
 </div>
-<script src="/game/latest.js"></script>
 
+<script src="/game/latest.js"></script>
 """
 
 TARGET_DIR = "game/posts/minecraft"
@@ -15,6 +14,7 @@ TARGET_DIR = "game/posts/minecraft"
 for root, dirs, files in os.walk(TARGET_DIR):
 
     for file in files:
+
         if not file.endswith(".html"):
             continue
 
@@ -23,25 +23,21 @@ for root, dirs, files in os.walk(TARGET_DIR):
         with open(path, "r", encoding="utf-8") as f:
             html = f.read()
 
-        # 既に追加済みならスキップ
         if 'id="latest-posts"' in html:
             continue
 
-        marker = """
-</div>
-</div>
-</body>
-"""
+        if "</body>" not in html:
+            print("bodyなし:", path)
+            continue
 
-        if marker in html:
-            html = html.replace(
-                marker,
-                INSERT_HTML + marker
-            )
+        html = html.replace(
+            "</body>",
+            INSERT_HTML + "\n</body>"
+        )
 
-            with open(path, "w", encoding="utf-8") as f:
-                f.write(html)
+        with open(path, "w", encoding="utf-8") as f:
+            f.write(html)
 
-            print("updated:", path)
+        print("updated:", path)
 
 print("完了")
