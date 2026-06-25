5df2ad43247fe2f8cb6bce54c1d0004f365f8a63
block-surprise <miyazaki902@yahoo.co.jp>
2026-06-25 18:25:41 +0900

    generate_game.py を更新

--- generate_game.py
+++ generate_game.py
@@ -374,13 +374,13 @@ h2 {{
 </div>
 
 </div>
+
+<script src="/game/latest.js"></script>
  <footer class="footer">
 
   <a href="https://htn-news.f5.si/privacy.html">・プライバシーポリシー</a>
   <a href="https://htn-news.f5.si/a10">・不適切な内容等削除申請フォーム</a>
 </footer>
-<script src="/game/latest.js"></script>
-
 </body>
 </html>
 """
