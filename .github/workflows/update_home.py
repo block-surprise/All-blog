93b950a1b4c0e886f1f90d4f3d349a11b59790a0
block-surprise <miyazaki902@yahoo.co.jp>
2026-06-25 18:20:57 +0900

    update_home.py を更新

--- .github/workflows/update_home.py
+++ .github/workflows/update_home.py
@@ -372,8 +372,8 @@ header h1 {{
 </div>
 <footer class="footer">
 
-  <a href="/privacy.html">プライバシーポリシー</a>
-  <a href="https://htn-news.f5.si/a10">不適切な内容等削除申請フォーム</a>
+  <a href="/privacy.html">・プライバシーポリシー</a>
+  <a href="https://htn-news.f5.si/a10">・不適切な内容等削除申請フォーム</a>
 </footer>
 </body>
 </html>
